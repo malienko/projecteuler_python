be4748242ae1ff049204f5cb980286a803151f93
Anton Malienko <malienko.a@hotmail.com>
2019-04-28 17:46:15 -0400

    Create problem15.py

---
+++ problem15.py
@@ -0,0 +1,12 @@
+'''
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+'''
+import math
+
+n = 20
+
+result = (math.factorial(2 * n))/(math.factorial(n)**2) # See central binomial coefficients (https://en.wikipedia.org/wiki/Central_binomial_coefficient)
+
+print(int(result))
